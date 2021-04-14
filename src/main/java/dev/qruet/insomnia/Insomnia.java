8d5402ad2b908853f5df8ab86102176b7fd7632f
qruet <geeveloper@gmail.com>
2021-04-14 13:12:49 -0400

    Simple first commit - general git setup and populate

---
+++ src/main/java/dev/qruet/insomnia/Insomnia.java
@@ -0,0 +1,11 @@
+package dev.qruet.insomnia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Insomnia extends JavaPlugin {
+
+    public void onEnable() {
+        getLogger().info("Enabled Insomnia.");
+    }
+
+}
